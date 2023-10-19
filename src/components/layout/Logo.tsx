67a72cd4e5a1b06de3a4ea8bc9e03a1d0391ff12
andihub <3052211+andihub@users.noreply.github.com>
2023-10-19 21:00:09 +0000

    favicons, logo

---
+++ src/components/layout/Logo.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <Image src="/apple-touch-icon.png" alt="Logo" width="64" height="64" />
+  );
+}
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,11 +1,17 @@
-import { Container, HStack, Heading, Spacer } from "@chakra-ui/react";
+import { Container, HStack, Heading, Spacer, Text } from "@chakra-ui/react";
 import SunMoonLightModeSwitch from "../SunMoonLightModeSwitch";
+import Logo from "./Logo";
 
 export const Navbar = () => {
   return (
     <Container>
       <HStack py="4" as="nav">
-        <Heading>Image API bulk downloader</Heading>
+        <Logo></Logo>
+        <Heading size={["sm", "md"]}>
+          <Text as="span" >
+            <Text as="span"  color="blue.500">I</Text>mages API bulk <Text as="span"  color="blue.500">D</Text>ownloader
+          </Text>
+        </Heading>
         <Spacer />
         <SunMoonLightModeSwitch />
       </HStack>
