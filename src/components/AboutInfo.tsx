199f298939ecb30c7dd2e71f8ada0e8692c4e43d
andihub <3052211+andihub@users.noreply.github.com>
2023-10-20 10:52:04 +0000

    fix type

--- src/components/AboutInfo.tsx
+++ src/components/AboutInfo.tsx
@@ -4,7 +4,7 @@ export default function AboutInfo() {
   return (
     <VStack gap="2" align="flex-start">
       <Text fontSize="sm">
-        This tool loads a the JSON content of an API, parses it and extracts the
+        This tool loads the JSON content of an API, parses it and extracts the
         image URL, displays all the images and provides the possibility to
         download a single image or download selected images at once as a ZIP
         file.
